7c7c8fd9fb3e61ae44cc4821301c53e297e0bd75
David <ga47rus@mytum.de>
2015-12-16 13:09:02 +0100

    Worked on Task 2 (Parking)

--- tas_autonomous_control/src/tas_autonomous_control_node.cpp
+++ tas_autonomous_control/src/tas_autonomous_control_node.cpp
@@ -7,7 +7,6 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "autonomous_control");
 
     control autonomous_control;
-    einparken parken;
 
     geometry_msgs::Vector3 regler;
 
@@ -21,29 +20,22 @@ int main(int argc, char** argv)
         }
 	else if (autonomous_control.control_Mode.data==2) {
 	    ROS_INFO("Parking Control!");
-	    if (parken.parking_started == false) {
-	    	parken.Start_parking();
-	    }
-	    else
-	    {
+	    
+		if(autonomous_control.cmd_linearVelocity>0)
+                {
+                    autonomous_control.control_servo.x = 1535;
+                }
+                else if(autonomous_control.cmd_linearVelocity<0)
+                {
+                    autonomous_control.control_servo.x = 1300;
+                }
+                else
+                {
+                    autonomous_control.control_servo.x = 1500;
+                }
 
-		    if(parken.fahre_vorwaerts)
-		    {
-		        autonomous_control.control_servo.x = 1530;
-		    }
-		    else if(parken.fahre_rueckwaerts)
-		    {
-		        autonomous_control.control_servo.x = 1300;
-		    }
-		    else if(parken.lenke_links)
-		    {
-		        autonomous_control.control_servo.y = 1000;
-		    }
-		    else if(parken.lenke_rechts)
-		    {
-			autonomous_control.control_servo.y = 2000;
-		    }
-	    }	
+                autonomous_control.control_servo.y = autonomous_control.cmd_steeringAngle;
+		
 	}
         else
         {
