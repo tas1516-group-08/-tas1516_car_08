a0da2be3a5517b55380dc9a556f687bdd0eb4061
tas1516-group-08 <tas1516group08@gmail.com>
2015-12-20 21:38:08 +0100

    Parking Simulation finished

---
+++ tas_autonomous_control_simulation/src/parking/parking.cpp
@@ -0,0 +1,57 @@
+#include "parking.h"
+
+// Kommentar
+parking::parking()
+{
+
+    cmd_parking_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 10);
+    laser_back_sub = n.subscribe<sensor_msgs::LaserScan>("scan_back",10, &parking::LaserBackCallback, this);
+    laser_front_sub = n.subscribe<sensor_msgs::LaserScan>("scan",10, &parking::LaserFrontCallback, this);
+
+	
+    wii_communication_sub = n.subscribe<std_msgs::Int16MultiArray>("wii_communication",1000,&parking::wiiCommunicationCallback, this);
+
+    park = false;
+	detect_edge =0;
+	fortschritt =0;
+}
+
+
+// Kommentar
+void parking::LaserBackCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+{
+    rangesPtr = &(msg->ranges)[0];
+	for (int i = 1; i <= 639; i++)
+	{
+		range_array_back[i] = msg->ranges[i];
+	}
+}
+
+// Kommentar
+void parking::LaserFrontCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+{
+    rangesPtr = &(msg->ranges)[0];
+	for (int i = 1; i <= 639; i++)
+	{
+		range_array_front[i] = msg->ranges[i];
+	}
+}
+
+// Kommentar
+// a flag method that tells us if we are controlling the car manually, automatically or in parking mode
+void parking::wiiCommunicationCallback(const std_msgs::Int16MultiArray::ConstPtr& msg)
+{
+    control_Mode.data = msg->data[0];
+	if (control_Mode.data == 2) 
+	{ //Parking Control active
+		park = true;
+	} 
+	else 
+	{
+		park = false;
+		detect_edge =0;
+		fortschritt =0;
+	}
+}
+
+
